229c5e4760fa94fe025be49a5f77f9187513610f
kclute627 <kyclutter@gmail.com>
2022-09-11 21:40:35 -0500

    Initial Commit

---
+++ UiKit LIst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiKit LIst
+//
+//  Created by Kyle  Clutter on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
